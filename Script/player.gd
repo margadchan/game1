2dd2bb8134b0778f8a33951b8d90127dd21e7023
margadchan <magymagy0816@gmail.com>
2025-09-27 19:08:03 -0400

    Initial commit

---
+++ Script/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED = 100
+
+func _physics_process(delta):
+	player_movement(delta)
+	move_and_slide()
+
+func player_movement(_delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = SPEED
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y = -SPEED
+		velocity.x = 0
+	else:
+		velocity = Vector2.ZERO
